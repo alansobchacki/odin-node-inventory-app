b968c29539484b1a606ed60ee461afb54b5ac8ed
Alan Sobchacki <a.sobchack@gmail.com>
2024-07-19 17:32:08 -0300

    implement a very basic app with a db that creates and reads new users.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db/queries");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", async function (req, res, next) {
+  try {
+    const usernames = await db.getAllUsernames();
+    console.log("usernames: ", usernames);
+    res.render("index", { title: "Express", usernames });
+  } catch (err) {
+    console.log("Error fetching usernames", err);
+    next(err);
+  }
+});
+
+/* GET add new user page */
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", async function (req, res, next) {
+  try {
+    const username = req.body.username;
+    await db.insertUsername(username);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Failed to add new user", err);
+    next(err);
+  }
 });
 
 module.exports = router;
